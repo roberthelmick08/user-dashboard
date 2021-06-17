98c2616bfef071f03fa5793677602c75b0fed867
Robert Helmick <robert.helmick.08@gmail.com>
2021-06-17 10:27:21 -0600

    component scaffolding & add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    AddEditUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
