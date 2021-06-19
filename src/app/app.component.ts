f9117a333f3737ac9633bcc66c3b4c00487af447
Robert Helmick <robert.helmick.08@gmail.com>
2021-06-19 14:50:28 -0600

    remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AddEditUserComponent } from './components/add-edit-user/add-edit-user.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { UserListComponent } from './components/user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
