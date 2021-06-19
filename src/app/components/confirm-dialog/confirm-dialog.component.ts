600096339fd38786e9555f87b210c37d8e707525
Robert Helmick <robert.helmick.08@gmail.com>
2021-06-19 14:35:23 -0600

    code cleanup

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,17 +1,16 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { User } from 'src/app/models/user-interface';
 
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss']
 })
-export class ConfirmDialogComponent implements OnInit {
+export class ConfirmDialogComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {title: string, message: string}) { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: { title: string, message: string }
+  ) { }
 
 }
--- src/app/models/user-interface.ts
+++ src/app/models/user-interface.ts
@@ -2,7 +2,5 @@ export interface User {
     id: string;
     email: string;
     username: string;
-    firstname?: string;
-    lastname?: string;
     activated: boolean;
 }
